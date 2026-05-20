451b4e150094e850175be1f5938304563ae670db
suluoimran <suluoimran@gmail.com>
2026-05-20 17:10:59 +0300

    add python exercise files

---
+++ even_odd.py
@@ -0,0 +1,5 @@
+number=int(input("enter a number: "))
+if number % 2 == 0:
+   print("UR NUMBER IS EVEN")
+else:
+   print("UR NUMBER IS ODD")
---
+++ multiplication.py
@@ -0,0 +1,3 @@
+m=int(input("enter a number: "))
+for i in range(1,11):
+    print(f"{m}*{i}={m*i}")
